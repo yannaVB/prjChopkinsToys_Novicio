4375c2a56287943274432359eabbe9a9fe66b7e3
Anna <novicioannamarie22@gmail.com>
2021-02-05 00:05:20 +0800

    initial commit

---
+++ prjChopkinsToys_Novicio/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.txtSalesTotal2 = New System.Windows.Forms.TextBox()
+        Me.txtSalesTotal = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtShipping = New System.Windows.Forms.TextBox()
+        Me.lblShipping = New System.Windows.Forms.Label()
+        Me.txt2pack2 = New System.Windows.Forms.TextBox()
+        Me.txt5pack2 = New System.Windows.Forms.TextBox()
+        Me.txt12pack2 = New System.Windows.Forms.TextBox()
+        Me.txt2pack1 = New System.Windows.Forms.TextBox()
+        Me.txt5pack1 = New System.Windows.Forms.TextBox()
+        Me.txt12pack1 = New System.Windows.Forms.TextBox()
+        Me.lbl2pack = New System.Windows.Forms.Label()
+        Me.lbl5pack = New System.Windows.Forms.Label()
+        Me.lbl12pack = New System.Windows.Forms.Label()
+        Me.txtDo = New System.Windows.Forms.TextBox()
+        Me.lblDo = New System.Windows.Forms.Label()
+        Me.lblInvoice = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(222, 285)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 39
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(131, 285)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 38
+        Me.btnClear.Text = "C&lear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(38, 285)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 37
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'txtSalesTotal2
+        '
+        Me.txtSalesTotal2.BackColor = System.Drawing.SystemColors.Info
+        Me.txtSalesTotal2.Location = New System.Drawing.Point(181, 242)
+        Me.txtSalesTotal2.Name = "txtSalesTotal2"
+        Me.txtSalesTotal2.ReadOnly = True
+        Me.txtSalesTotal2.Size = New System.Drawing.Size(92, 20)
+        Me.txtSalesTotal2.TabIndex = 36
+        '
+        'txtSalesTotal
+        '
+        Me.txtSalesTotal.Location = New System.Drawing.Point(119, 242)
+        Me.txtSalesTotal.Name = "txtSalesTotal"
+        Me.txtSalesTotal.ReadOnly = True
+        Me.txtSalesTotal.Size = New System.Drawing.Size(56, 20)
+        Me.txtSalesTotal.TabIndex = 35
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(36, 242)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 15)
+        Me.Label1.TabIndex = 34
+        Me.Label1.Text = "Sales totals:"
+        '
+        'txtShipping
+        '
+        Me.txtShipping.BackColor = System.Drawing.SystemColors.Info
+        Me.txtShipping.Location = New System.Drawing.Point(181, 218)
+        Me.txtShipping.Name = "txtShipping"
+        Me.txtShipping.ReadOnly = True
+        Me.txtShipping.Size = New System.Drawing.Size(92, 20)
+        Me.txtShipping.TabIndex = 33
+        '
+        'lblShipping
+        '
+        Me.lblShipping.AutoSize = True
+        Me.lblShipping.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblShipping.Location = New System.Drawing.Point(79, 218)
+        Me.lblShipping.Name = "lblShipping"
+        Me.lblShipping.Size = New System.Drawing.Size(59, 15)
+        Me.lblShipping.TabIndex = 32
+        Me.lblShipping.Text = "Shipping:"
+        '
+        'txt2pack2
+        '
+        Me.txt2pack2.BackColor = System.Drawing.SystemColors.Info
+        Me.txt2pack2.Location = New System.Drawing.Point(181, 175)
+        Me.txt2pack2.Name = "txt2pack2"
+        Me.txt2pack2.ReadOnly = True
+        Me.txt2pack2.Size = New System.Drawing.Size(92, 20)
+        Me.txt2pack2.TabIndex = 31
+        '
+        'txt5pack2
+        '
+        Me.txt5pack2.BackColor = System.Drawing.SystemColors.Info
+        Me.txt5pack2.Location = New System.Drawing.Point(181, 149)
+        Me.txt5pack2.Name = "txt5pack2"
+        Me.txt5pack2.ReadOnly = True
+        Me.txt5pack2.Size = New System.Drawing.Size(92, 20)
+        Me.txt5pack2.TabIndex = 30
+        '
+        'txt12pack2
+        '
+        Me.txt12pack2.BackColor = System.Drawing.SystemColors.Info
+        Me.txt12pack2.Location = New System.Drawing.Point(181, 123)
+        Me.txt12pack2.Name = "txt12pack2"
+        Me.txt12pack2.ReadOnly = True
+        Me.txt12pack2.Size = New System.Drawing.Size(92, 20)
+        Me.txt12pack2.TabIndex = 29
+        '
+        'txt2pack1
+        '
+        Me.txt2pack1.Location = New System.Drawing.Point(119, 175)
+        Me.txt2pack1.Name = "txt2pack1"
+        Me.txt2pack1.Size = New System.Drawing.Size(56, 20)
+        Me.txt2pack1.TabIndex = 28
+        '
+        'txt5pack1
+        '
+        Me.txt5pack1.Location = New System.Drawing.Point(119, 149)
+        Me.txt5pack1.Name = "txt5pack1"
+        Me.txt5pack1.Size = New System.Drawing.Size(56, 20)
+        Me.txt5pack1.TabIndex = 27
+        '
+        'txt12pack1
+        '
+        Me.txt12pack1.Location = New System.Drawing.Point(119, 123)
+        Me.txt12pack1.Name = "txt12pack1"
+        Me.txt12pack1.Size = New System.Drawing.Size(56, 20)
+        Me.txt12pack1.TabIndex = 26
+        '
+        'lbl2pack
+        '
+        Me.lbl2pack.AutoSize = True
+        Me.lbl2pack.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl2pack.Location = New System.Drawing.Point(65, 173)
+        Me.lbl2pack.Name = "lbl2pack"
+        Me.lbl2pack.Size = New System.Drawing.Size(48, 15)
+        Me.lbl2pack.TabIndex = 25
+        Me.lbl2pack.Text = "&2 pack:"
+        '
+        'lbl5pack
+        '
+        Me.lbl5pack.AutoSize = True
+        Me.lbl5pack.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl5pack.Location = New System.Drawing.Point(65, 149)
+        Me.lbl5pack.Name = "lbl5pack"
+        Me.lbl5pack.Size = New System.Drawing.Size(48, 15)
+        Me.lbl5pack.TabIndex = 24
+        Me.lbl5pack.Text = "&5 pack:"
+        '
+        'lbl12pack
+        '
+        Me.lbl12pack.AutoSize = True
+        Me.lbl12pack.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl12pack.Location = New System.Drawing.Point(58, 126)
+        Me.lbl12pack.Name = "lbl12pack"
+        Me.lbl12pack.Size = New System.Drawing.Size(55, 15)
+        Me.lbl12pack.TabIndex = 23
+        Me.lbl12pack.Text = "&12 pack:"
+        '
+        'txtDo
+        '
+        Me.txtDo.Location = New System.Drawing.Point(155, 97)
+        Me.txtDo.Name = "txtDo"
+        Me.txtDo.Size = New System.Drawing.Size(120, 20)
+        Me.txtDo.TabIndex = 22
+        '
+        'lblDo
+        '
+        Me.lblDo.AutoSize = True
+        Me.lblDo.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDo.Location = New System.Drawing.Point(65, 97)
+        Me.lblDo.Name = "lblDo"
+        Me.lblDo.Size = New System.Drawing.Size(84, 15)
+        Me.lblDo.TabIndex = 21
+        Me.lblDo.Text = "&Date ordered:"
+        '
+        'lblInvoice
+        '
+        Me.lblInvoice.AutoSize = True
+        Me.lblInvoice.Font = New System.Drawing.Font("Arial", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInvoice.Location = New System.Drawing.Point(98, 33)
+        Me.lblInvoice.Name = "lblInvoice"
+        Me.lblInvoice.Size = New System.Drawing.Size(126, 36)
+        Me.lblInvoice.TabIndex = 20
+        Me.lblInvoice.Text = "Invoice"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(333, 341)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtSalesTotal2)
+        Me.Controls.Add(Me.txtSalesTotal)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtShipping)
+        Me.Controls.Add(Me.lblShipping)
+        Me.Controls.Add(Me.txt2pack2)
+        Me.Controls.Add(Me.txt5pack2)
+        Me.Controls.Add(Me.txt12pack2)
+        Me.Controls.Add(Me.txt2pack1)
+        Me.Controls.Add(Me.txt5pack1)
+        Me.Controls.Add(Me.txt12pack1)
+        Me.Controls.Add(Me.lbl2pack)
+        Me.Controls.Add(Me.lbl5pack)
+        Me.Controls.Add(Me.lbl12pack)
+        Me.Controls.Add(Me.txtDo)
+        Me.Controls.Add(Me.lblDo)
+        Me.Controls.Add(Me.lblInvoice)
+        Me.Name = "Form1"
+        Me.Text = "Chopkins Toys Solution "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents txtSalesTotal2 As TextBox
+    Friend WithEvents txtSalesTotal As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtShipping As TextBox
+    Friend WithEvents lblShipping As Label
+    Friend WithEvents txt2pack2 As TextBox
+    Friend WithEvents txt5pack2 As TextBox
+    Friend WithEvents txt12pack2 As TextBox
+    Friend WithEvents txt2pack1 As TextBox
+    Friend WithEvents txt5pack1 As TextBox
+    Friend WithEvents txt12pack1 As TextBox
+    Friend WithEvents lbl2pack As Label
+    Friend WithEvents lbl5pack As Label
+    Friend WithEvents lbl12pack As Label
+    Friend WithEvents txtDo As TextBox
+    Friend WithEvents lblDo As Label
+    Friend WithEvents lblInvoice As Label
+End Class
---
+++ prjChopkinsToys_Novicio/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Const strPROMPT As String = "Enter Shipping Fee:"
+        Const strTITLE As String = "Shipping"
+
+        Const dec12Pack As Decimal = 14.99D
+        Const dec5Pack As Decimal = 6.99D
+        Const dec2Pack As Decimal = 2.5D
+
+        Dim int12Pack As Integer
+        Dim int5pack As Integer
+        Dim int2Pack As Integer
+        Dim intTotalOrders As Integer
+
+        Dim decTotalAmount As Decimal
+        Dim dec12Total As Decimal
+        Dim dec5Total As Decimal
+        Dim dec2Total As Decimal
+        Dim decShipping As Decimal
+        'shipping'
+        decShipping = Convert.ToDecimal(InputBox(strPROMPT, strTITLE))
+        txtShipping.Text = decShipping.ToString("C2")
+
+        Integer.TryParse(txt12pack1.Text, int12Pack)
+        Integer.TryParse(txt5pack1.Text, int5pack)
+        Integer.TryParse(txt2pack1.Text, int2Pack)
+        intTotalOrders = int12Pack + int5pack + int2Pack
+        txtSalesTotal.Text = intTotalOrders.ToString
+
+        dec12Total = int12Pack * dec12Pack
+        dec5Total = int5pack * dec5Pack
+        dec2Total = int2Pack * dec2Pack
+
+        txt12pack2.Text = dec12Total.ToString("C2")
+        txt5pack2.Text = dec5Total.ToString("C2")
+        txt2pack2.Text = dec2Total.ToString("C2")
+
+        decTotalAmount = dec12Total + dec5Total + dec2Total + decShipping
+        txtSalesTotal2.Text = decTotalAmount.ToString("C2")
+        txtSalesTotal.Text = intTotalOrders.ToString()
+
+
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Application.CurrentCulture = New Globalization.CultureInfo("en-US")
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txt12pack1.Clear()
+        txt12pack2.Clear()
+        txt5pack1.Clear()
+        txt5pack2.Clear()
+        txt2pack2.Clear()
+        txt2pack1.Clear()
+        txtSalesTotal.Clear()
+        txtSalesTotal2.Clear()
+        txtShipping.Clear()
+
+    End Sub
+End Class
